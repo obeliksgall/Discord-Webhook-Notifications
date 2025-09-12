225a9b581c71be9536e965bad89224a856314b33
obeliksgall <mosiu24@gmail.com>
2025-09-12 20:25:18 +0200

    Update Discord-Webhook-Notifications.ps1
    Added parameter support:
    -Mode turnon
    -Mode turnoff

--- Discord-Webhook-Notifications.ps1
+++ Discord-Webhook-Notifications.ps1
@@ -1,31 +1,52 @@
 # source https://github.com/obeliksgall/Discord-Webhook-Notifications/tree/main
-# version 1.0
-$webhookUri = '' # Enter Discord Webhook Address between apostrophe
+# version 1.1
 
-$data = Get-Date -Format "yyyy-MM-dd HH:mm:ss" # Get current date and time
-$hostname = $env:COMPUTERNAME # Get the computer name
-$timezone = [System.TimeZoneInfo]::Local.Id # Download your computer's timezone
-$ipAddress = (Get-NetIPAddress | Where-Object { $_.AddressFamily -eq "IPv4" -and $_.PrefixOrigin -eq "Dhcp" }).IPAddress # Getting the current IPv4 address
-$footerTime = (Get-Date).AddMinutes(-1).ToString("HH:mm") # Get current time minus 1 minute
+#how to run example | jak uruchomic
+#powershell.exe -ExecutionPolicy Bypass -File ".\Discord-Webhook-Notifications.ps1" -Mode turnon
+#powershell.exe -ExecutionPolicy Bypass -File ".\Discord-Webhook-Notifications.ps1" -Mode turnoff
+
+param (
+    [Parameter(Mandatory=$true)]
+    [ValidateSet("turnon", "turnoff")]
+    [string]$Mode
+)
+
+$webhookUri = 'https://discord.com/api/webhooks/...' # Enter Discord Webhook Address | Wstaw adres webhooka Discord
+
+$data = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+$hostname = $env:COMPUTERNAME
+$timezone = [System.TimeZoneInfo]::Local.Id
+$ipAddress = (Get-NetIPAddress | Where-Object { $_.AddressFamily -eq "IPv4" -and $_.PrefixOrigin -eq "Dhcp" }).IPAddress
+
+# Settings from mode | Ustawienia zalezne od trybu
+if ($Mode -eq "turnon") {
+    $footerTime = (Get-Date).AddMinutes(-1).ToString("HH:mm")
+    $username = "$hostname ON - $data"
+    $title = "Your service $hostname is up!"
+    $color = 65280 # Green | Zielony
+} elseif ($Mode -eq "turnoff") {
+    $footerTime = (Get-Date).ToString("HH:mm")
+    $username = "$hostname OFF - $data"
+    $title = "Your service $hostname is down!"
+    $color = 16711680 # Red | Czerwony
+}
+
 
 $Body = @{
-    "username" = "$hostname ON - $data"
+    "username" = $username
     "content" = ""
     "tts" = $false
     "embeds" = @(
         @{
-            "color" = 65280
-			#16711680 # Color red
-			#65280 # Green color
-            "title" = "Your service $hostname is up!"
-            #"description" = "$hostname"
+            "color" = $color
+            "title" = $title
             "fields" = @(
                 @{
                     "name" = "Computer Name"
                     "value" = "$hostname"
                     "inline" = $false
                 },
-				@{
+                @{
                     "name" = "Time ($timezone)"
                     "value" = "$data"
                     "inline" = $false
@@ -38,13 +59,12 @@ $Body = @{
             )
             "footer" = @{
                 "text" = "Today $footerTime`nSystem Notification`nSent from $hostname"
-            }			
+            }
         }
     )
 }
 
 $BodyJson = $Body | ConvertTo-Json -Depth 10
-
 $BodyBytes = [System.Text.Encoding]::UTF8.GetBytes($BodyJson)
 
 Invoke-RestMethod -Uri $webhookUri -Method 'Post' -Body $BodyBytes -ContentType 'application/json'
